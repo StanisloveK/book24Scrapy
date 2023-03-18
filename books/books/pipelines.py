b978b312b2a45dc14eff82b0036fa2bc37333a67
StanisloveK <95179340+StanisloveK@users.noreply.github.com>
2023-03-18 14:22:51 +0200

    Update pipelines.py

--- books/books/pipelines.py
+++ books/books/pipelines.py
@@ -19,7 +19,14 @@ class BooksPipeline:
     def process_item(self, item, spider):
         """Process item."""
         collection = self.mongo_base[spider.name]
-
+        item['own_price'] = item['own_price'].replace(r'\xa0', '').replace(r' ', '').replace(r'₽', '') + '₽'
+        if item['sale_price']:
+            item['sale_price'] = item['sale_price'].replace(r'\xa0', '').replace(r' ', '').replace(r'₽', '') + '₽'
+        else:
+            item['sale_price'] = 'Нет скидки'
+        if item['rating']:
+            item['rating'] = float(item['rating'])
+        else:
+            item['rating'] = 'Нет рейтинга'
         collection.update_one(item, {'$setOnInsert': item}, upsert=True)
-
         return item
